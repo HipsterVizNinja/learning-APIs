c47e0e1734d022ba4c86720f4bb16ba0663e4c02
Miller,Sean <Sean.Miller@Cerner.com>
2022-01-07 21:10:16 -0600

    Good Times

---
+++ Twitter-DM.py
@@ -0,0 +1,30 @@
+import tweepy
+
+# Your app's API/consumer key and secret can be found under the Consumer Keys
+# section of the Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+consumer_key = ""
+consumer_secret = ""
+
+# Your account's (the app owner's account's) access token and secret for your
+# app can be found under the Authentication Tokens section of the
+# Keys and Tokens tab of your app, under the
+# Twitter Developer Portal Projects & Apps page at
+# https://developer.twitter.com/en/portal/projects-and-apps
+access_token = ""
+access_token_secret = ""
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+# the screen name of the user
+
+
+# fetching the user
+user = api.get_user(screen_name = "TWITTER-HANDLE")
+text = "SEND THIS DM"
+
+# direct_message = api.send_direct_message(user.id, text)
+print(user.id)
